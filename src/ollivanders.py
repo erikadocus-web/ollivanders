e0501ffdd5d47267ca6cfebff276ce44f2c6ee3f
ErikaSousa <erika.docus@gmail.com>
2026-02-23 14:17:09 +0100

    feat(NormalItem): Añadir clase NormalItem

--- src/ollivanders.py
+++ src/ollivanders.py
@@ -11,6 +11,21 @@ class Ollivanders():
         def updateQuality(self):
             pass
 
+    class NormalItem(Item):
+        def __init__(self, name, sellIn, quality):
+            self.name = name
+            self.sellIn = sellIn
+            self.quality = quality
+
+        def updateQuality(self):
+            q = 1
+            if self.sellIn >= 0:
+                self.quality = self.quality - q * 1
+            else:
+                self.quality = self.quality - q *2
+            
+            print(f"Calidad nueva de {self.name} es {self.quality}")
+
 if __name__ == "__main__": 
     Ollivanders = Ollivanders()
     normalItem = Ollivanders.NormalItem("item1", 10, 20)
