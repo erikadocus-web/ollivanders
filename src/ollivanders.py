f6684e4abd50dcc601bf4e8bce6b0188d762523c
ErikaSousa <erika.docus@gmail.com>
2026-02-26 09:20:23 +0100

    feat(ollivanders): Añadir clases

--- src/ollivanders.py
+++ src/ollivanders.py
@@ -1,34 +1,55 @@
 class Ollivanders():
+
+    def __init__(self, items):
+        self.items = items
+    def updateQuality(self):
+        for item in self.items:
+            item.updateQuality()
+
+class Interfaz():
     def __init__(self):
-        self.inventory = {}
+        pass
 
-    class Item():
-        def __init__(self):
-            self.name = ""
-            self.sellIn = 0
-            self.quality = 0
+class Item:
+    def __init__(self,name, sellIn, quality):
+        self.name = name
+        self.sellIn = sellIn
+        self.quality = quality
+        
+    @property
+    def quality(self):
+        return self._quality
+        
+    @quality.setter
+    def quality(self,quality):
+        self._quality = max(0, min(50, quality))
 
-        def updateQuality(self):
-            pass
 
-    class NormalItem(Item):
-        def __init__(self, name, sellIn, quality):
-            self.name = name
-            self.sellIn = sellIn
-            self.quality = quality
-
-        def updateQuality(self):
-            q = 1
-            if self.sellIn >= 0:
-                self.quality = self.quality - q * 1
-            else:
-                self.quality = self.quality - q *2
+class NormalItem(Item, Interfaz):
+    def setSellIn(self):
+        self.sellIn = self.sellIn - 1
             
-            print(f"Calidad nueva de {self.name} es {self.quality}")
+    def updateQuality(self):
+        self.setSellIn()
+        if self.sellIn > 0:
+            self.quality = self.quality - 1
+        else:
+            self.quality = self.quality - 2
+
+class AgedBrie(NormalItem):
+    pass
+
+class Conjured(NormalItem):
+    pass
+
+class Backstage(NormalItem):
+    pass
+
+class Sulfuras(NormalItem):
+    pass
 
 if __name__ == "__main__": 
-    Ollivanders = Ollivanders()
-    normalItem = Ollivanders.NormalItem("item1", 10, 20)
-    normalItem.updateQuality()
+    elixir = NormalItem("elixir", 50, 15)
+    print(elixir.quality)
 
     
