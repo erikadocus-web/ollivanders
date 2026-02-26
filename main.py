3141c8ec37d7f211f5e38a35eaa3a889ae591c78
ErikaSousa <erika.docus@gmail.com>
2026-02-26 10:16:54 +0100

    feat(main): Añadir items y contador de días

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from src.ollivanders import Ollivanders, NormalItem, AgedBrie, Backstage, Conjured, Sulfuras
+
 def main():
     print("Bienvenido a la tienda de Ollivanders")
 
 if __name__ == "__main__":
-    main()
+    items = [
+        NormalItem("Varita de roble", 10, 20),
+        AgedBrie("Reliquia encantada antigua", 2, 0),
+        Backstage("Entrada al Torneo de los Tres Magos", 15, 20),
+        Conjured("Poción conjurada", 3, 6),
+        Sulfuras("Sulfuras, la varita eterna", 0, 80)
+    ]
+
+    tienda = Ollivanders(items)
+
+    for day in range(31):
+        print(f"\nDia {day}")
+        for item in items:
+            print(f"{item.name}, SellIn: {item.sellIn}, Quality: {item.quality}")
+
+        tienda.updateQuality()
